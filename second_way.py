7b860f45401107728b1d1cdb4f16452ded1be871
Ben Doherty <ben@notionparallax.co.uk>
2024-08-30 12:58:51 +1000

    new method

--- second_way.py
+++ second_way.py
@@ -5,6 +5,7 @@ import Grasshopper.Kernel as ghk
 gh_doc = gh.Instances.ActiveCanvas.Document
 
 subgraphs = ""
+connections_dot = ""
 
 for index, obj in enumerate(gh_doc.Objects):
     node_id = obj.InstanceGuid.ToString()
@@ -22,23 +23,37 @@ for index, obj in enumerate(gh_doc.Objects):
             node_name = '"{}_in_{}"'.format(input_param.NickName, node_id)
             params_dot += '{} [label="{} \\n{}"];\n    '.format(node_name, input_param.NickName, type(input_param).__name__)
             internal_connections_dot += '{} -> "{}";\n    '.format(node_name, fq_name)
+            
+            # Add connections from sources to this input
+            for source in input_param.Sources:
+                source_node_name = '"{}_out_{}"'.format(source.NickName, source.InstanceGuid.ToString())
+                connections_dot += '{} -> {};\n    '.format(source_node_name, node_name)
         
         # Enumerate outputs
         for output_param in obj.Params.Output:
             node_name = '"{}_out_{}"'.format(output_param.NickName, node_id)
-            params_dot += '{} [label="{} \\n{}"];\n    '.format(node_name, output_param.NickName, type(input_param).__name__)
+            params_dot += '{} [label="{} \\n{}"];\n    '.format(node_name, output_param.NickName, type(output_param).__name__)
             internal_connections_dot += '"{}" -> {};\n    '.format(fq_name, node_name)
-
+            
+            # Add connections from this output to recipients
+            for recipient in output_param.Recipients:
+                recipient_node_name = '"{}_in_{}"'.format(recipient.NickName, recipient.InstanceGuid.ToString())
+                connections_dot += '{} -> {};\n    '.format(node_name, recipient_node_name)
+        
         subgraphs += """
 subgraph "cluster_{0}" {{
     node [style=filled];
+    nodesep="0.05"
     label = "{1}";
     {2}
     
     {3}
   }}""".format(node_id, nn, params_dot, internal_connections_dot)
+  
+  
     else:
-        print("____does not have Params property")
+        
+        # print("____does not have Params property")
         if hasattr(obj, 'Sources'):
             # Access the value
             try:
@@ -46,16 +61,40 @@ subgraph "cluster_{0}" {{
             except:
                 value = obj.VolatileData
             print("____Value: {}".format(value))
-            
+            internal_connections_dot = ""
+            params_dot = '"{}" [label="{}\\nValue: {}", fillcolor="pink", fontsize=20];\n    '.format(fq_name, nn, value)
+
+            if obj.Sources:
+                internal_connections_dot += '{} -> "{}";\n    '.format(node_name, fq_name)
+            if obj.Recipients:
+                internal_connections_dot += '"{}" -> {};\n    '.format(fq_name, node_name)
+                
             # Inspect connections
             for source in obj.Sources:
                 print("____Connected to: {}, Type: {}".format(source.NickName, type(source).__name__))
+                source_node_name = '"{}_out_{}"'.format(source.NickName, source.InstanceGuid.ToString())
+                connections_dot += '{} -> "{}";\n    '.format(source_node_name, fq_name)
             for recipient in obj.Recipients:
                 print("____Recipient: {}, Type: {}".format(recipient.NickName, type(recipient).__name__))
+                recipient_node_name = '"{}_in_{}"'.format(recipient.NickName, recipient.InstanceGuid.ToString())
+                connections_dot += '"{}" -> {};\n    '.format(fq_name, recipient_node_name)
                 
-                
+            subgraphs += """
+subgraph "cluster_{0}" {{
+    node [style=filled];
+    nodesep="0.05"
+    label = "{1}";
+    {2}
+    
+    {3}
+  }}""".format(node_id, nn, params_dot, internal_connections_dot)
+
 graph = """digraph G {{
     rankdir = LR;
     
-    {}
-}}""".format(subgraphs)
+    {0}
+    
+    {1}
+}}""".format(subgraphs, connections_dot)
+
+print(graph)
