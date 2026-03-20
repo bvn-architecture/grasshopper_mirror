8c508ce6a41333b982754adcb0ee4ce558cd41e0
Ben Doherty <ben@notionparallax.co.uk>
2026-03-20 15:30:14 +1100

    fix pivot error

--- lib/mirror_scaffold.py
+++ lib/mirror_scaffold.py
@@ -129,6 +129,8 @@ def _find_by_nickname_exact(gh_doc, nickname):
 
 def _place(obj, x, y):
     """Set the pivot position of an object."""
+    if obj.Attributes is None:
+        obj.CreateAttributes()
     obj.Attributes.Pivot = sd.PointF(float(x), float(y))
 
 
